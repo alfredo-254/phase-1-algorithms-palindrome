2bf1877d693fa3d7acba1e663b7d184684da6c75
alfred kivuva <kivuvakioko97@gmail.com>
2024-01-14 00:06:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 function isPalindrome(word) {
   // Write your algorithm here
 }
+function isPalindrome(str) {
+  // Convert the string to lowercase
+  str = str.toLowerCase();
+  
+  // Remove non-alphabetic characters
+  str = str.replace(/[^a-z]/g, '');
 
-/* 
-  Add your pseudocode here
-*/
+  // Check if the string is equal to its reverse
+  return str === str.split('').reverse().join('');
+}
+// Explanation of solution through example
+// Example usage:
+const inputString = "racecar";
+const result = isPalindrome(inputString);
 
-/*
-  Add written explanation of your solution here
-*/
+if (result) {
+  console.log(`${inputString} is a palindrome.`);
+} else {
+  console.log(`${inputString} is not a palindrome.`);
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
+  console.log("=>", isPalindrome("kayak"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("level"));
+
+  console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("lemon"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("mother"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
